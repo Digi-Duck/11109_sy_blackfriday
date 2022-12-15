262e60e808aa5ca85414ac0f5a7d291cabef57ca
Nsstring-game <a0976151179@gmail.com>
2022-12-15 15:13:43 +0800

    test

--- all.js
+++ all.js
@@ -25,11 +25,42 @@
 
   const nav = document.querySelector('#nav');
   const banner = document.querySelector('#banner');
-
-  window.addEventListener('scroll', function(e) {
+  const cards = document.querySelectorAll('#main-card .card')
+  
+  function scroll() {
     if(scrollY+80 > banner.offsetHeight) {
       nav.classList.add('fixed');
     }else {
       nav.classList.remove('fixed');
     }
+    cards.forEach((item) => {
+      console.log(scrollY, item.offsetTop);
+      
+      if(scrollY+500 > item.offsetTop){
+        item.querySelector('.pic').classList.add('fade');
+        item.querySelector('.card-body').classList.add('opc');
+      }
+    })
+  }
+  scroll();
+  window.addEventListener('scroll', scroll)
+
+
+
+  const imgBox = document.querySelectorAll('.imgBox');
+  const bannerImg = document.querySelector('#banner');
+
+  imgBox.forEach(function(item,index) {
+    imgBox[index].addEventListener('click',  function() {
+      for(let i = 0; i < imgBox.length; i++){
+        imgBox[i].className = 'imgBox';
+      }
+      this.className = 'imgBox active';
+      bannerImg.style.background = `url('${this.querySelector('img').getAttribute('src')}')`
+      console.log(this.querySelector('img').getAttribute('src'));
+    })
+  })
+  let x = 0;
+  setInterval(function() {
+    
   })
